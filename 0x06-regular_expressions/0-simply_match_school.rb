73031e62c44c0aba3617a380f672ccea3db69417
Yohannes Alemu <you@example.com>
2024-03-01 17:41:31 -0800

    Yohannes Alemu

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,7 @@
 #!/usr/bin/env ruby
 
-puts ARGV[0].scan(/School/).join
+# Using Oniguruma regular expression library
+regex = /School/
+
+# Accept one argument and pass it to the regular expression matching method
+puts ARGV[0].scan(regex).join
