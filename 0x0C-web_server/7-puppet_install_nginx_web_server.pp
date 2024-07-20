04241a9382a58b7aff56641fbad94bcfdd274192
yohannes4321 <alemuyohannes960@gmail.com>
2024-07-20 15:54:57 +0300

    dsss

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,26 @@
-# Install Nginx package
+# Automating project requirements using Puppet
+
 package { 'nginx':
   ensure => installed,
 }
 
-# Configure Nginx to redirect HTTP requests to a specific URL
-file_line { 'redirect_to_github':
-  ensure  => present,
-  path    => '/etc/nginx/sites-enabled/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^ /redirect_me https://github.com/yohannes4321 permanent;',
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => file,
+  content => '
+    server {
+      listen 80 default_server;
+      rewrite ^/redirect_me https://www.github.com/besthor permanent;
+    }
+  ',
+  notify  => Service['nginx'],
 }
 
-# Ensure the index.html file is present
-file { 'index_html':
-  path    => '/var/www/html/index.html',
+file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
-# Ensure Nginx service is running
 service { 'nginx':
-  ensure => running,
+  ensure  => running,
   require => Package['nginx'],
 }
+
